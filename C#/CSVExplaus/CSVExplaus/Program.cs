a3bcd09534068e288d05327c5f3e4cad1f077803
Hein Wai Htet <cuheinwaihtet@gmail.com>
2023-03-11 17:28:51 +0630

    adding csv,pdf export using explaus c#

---
+++ C#/CSVExplaus/CSVExplaus/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EPPlusSamples.LoadDataFromCsvFilesIntoTables;
+
+Console.WriteLine("Hello, World!");
+string[] lines =
+    {
+    "First line", "Second line", "Third line"
+};
+
+var directory = @"D:\IT Test\CSV\WriteLines.txt";
+var filename = string.Format(directory, "");
+int count = 1;
+while (File.Exists(filename))
+{
+    String[] path = filename.Split('.');
+    filename = @"D:\IT Test\CSV\WriteLines" + "(" + (count++) + ")" + "." + ".txt";
+}
+
+
+await File.WriteAllLinesAsync(filename, lines);
+Console.WriteLine("End");
