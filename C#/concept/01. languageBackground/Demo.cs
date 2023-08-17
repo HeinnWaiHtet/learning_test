232d052070a2f76dcb741bc1ecb85324466d9710
Hein Wai Htet <hein.wai.htet@jrce.co.jp>
2023-08-17 10:45:08 +0900

    adding c# project and how work c# code node and demo

---
+++ C#/concept/01. languageBackground/Demo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace concept._01._languageBackground
+{
+    class Demo
+    {
+        private string Name { get; set; }
+        private string Description { get; set; }
+
+        public Demo(string name, string desc)
+        {
+            this.Name = name;
+            this.Description = desc;
+        }
+
+        public string GetDetail() => $"{Name} : {Description}";
+    }
+
+    class Entry
+    {
+        public static void Main(string[] args)
+        {
+            var demo = new Demo("DEMO", "CHECK LANGUAGE BACKEND");
+            Console.WriteLine(demo.GetDetail());
+            Console.ReadLine();
+        }
+    }
+}
